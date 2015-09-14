1bd7257ac0d6fabdb6e2f1e7c40167e424a14647
ShermanDrake <shermandrake@comcast.net>
2015-09-14 12:13:15 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//var user_name = prompt('what is your name?' , 'type it here.');
+//document.write('Hello ' + user_name + '. Welcome to my page!');
+
+//for(var i = 0; i <= 500; i+=100) {
+//	console.log(i);
+
+//}
+
+// for(var i = 1; i <= 64; i=i*2) {
+// 	console.log(i);
+
+// }
+
+// for(var i = 0; i <= 10; i=i+2) {
+// console.log(i);
+
+// }
+
+// for(var i = 3; i <= 15; i=i+3) {
+// console.log(i);
+
+// }
+
+// for(var i = 9; i >= 0; i=i-1) {
+// console.log(i);
+
+// }
+
+for(var i = 0; i <= 3; i=i+1) {
+console.log(i);
+
+}
+
